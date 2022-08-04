71813e64364afce83d6c2b112fc7fd656317b52b
Cristian Calderon <cristianfab90@gmail.com>
2022-08-04 14:04:13 +0200

    restart project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
 
-import { useState } from "react"
-const App = () => {
-    const [counter, setCounter] = useState(0);
-    const [values, setValues] = useState();
-
-    const handleClick = () => {
-        setCounter(counter + 1)
-        setValues(values.concat(counter))
-    }
-    return (
-        <div className="container">
-
-            <h1>Hola Cristian</h1>
-            <h1>{counter}</h1>
-            <button onClick={handleClick}>Press</button>
-        </div>
-    )
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-
-
-export default App
-
-
-
-
+export default App;
