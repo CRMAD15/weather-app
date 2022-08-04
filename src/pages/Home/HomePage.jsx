abb602502f73f2c0234ad101bc510737775afec5
Cristian Calderon <cristianfab90@gmail.com>
2022-08-04 19:36:48 +0200

    Project get ready

---
+++ src/pages/Home/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CityWeather from "../../components/cityWeather";
+const HomePage = () => {
+    return (
+        <div>
+            <CityWeather />
+        </div>
+    );
+}
+
+export default HomePage;
